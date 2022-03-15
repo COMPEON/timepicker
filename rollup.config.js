d414bf43e2e7fa540f9a913d59050092180dc063
DominikFischer86 <dfischer@compeon.de>
2022-03-15 10:28:09 +0100

    Add globals and remove external-helpers-plugin

--- rollup.config.js
+++ rollup.config.js
@@ -14,20 +14,29 @@ export default {
       file: 'dist/timepicker.umd.js',
       name: 'timepicker',
       format: 'umd',
+      globals: {
+        react: "React",
+        "prop-types": "PropTypes",
+        "styled-components": "styled"
+      },
     },
     {
       file: 'dist/timepicker.es.js',
       name: 'timepicker',
       format: 'es',
-    }
+      globals: {
+        react: "React",
+        "prop-types": "PropTypes",
+        "styled-components": "styled"
+      },
+    },
   ],
   plugins: [
     babel({
       babelrc: false,
       exclude: 'node_modules/**',
       plugins: [
-        '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-external-helpers'
+        '@babel/plugin-proposal-class-properties'
       ],
       presets: [['@babel/env', { modules: false }], '@babel/react']
     })
